05d2972de2d3e5bea7987fb210310fb056cbb48b
jackjerrold <141284583+jackjerrold@users.noreply.github.com>
2026-01-14 17:17:22 +0000

    Update TeleOp.java

--- TeleOp.java
+++ TeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -12,32 +13,35 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 
-public class HelloRobot_TeleOp extends LinearOpMode {
+public class LAETeleop2026 extends LinearOpMode {
     private Blinker control_Hub;
     private DcMotor arm;
     private DcMotor leftFrontMotor;
     private DcMotor rightFrontMotor;
     private DcMotor leftBackMotor;
     private DcMotor rightBackMotor;
-    private Servo feedServo;
+    private CRServo feedServo;
     private DcMotor guideMotor;
     private DcMotor accelMotor;
 
-    boolean dpad_upPrevious = true;
-    boolean dpad_downPrevious = true;
+    boolean dpad_upPrevious = false;
+    boolean dpad_downPrevious = false;
+    boolean trigger = false;
+    boolean armGuide = false;
+    boolean armAccel = false;
 
-    private double guideSpeed = 0.2;
-    private double accelSpeed = 0.8
-
+    private double guideSpeed = -0.5;
+    private double accelSpeed = 0.8;
 
     @Override
+    
 public void runOpMode() {
         control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
         leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
         leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
         rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
         rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
-        feedServo = hardwareMap.get(Servo.class, "feedServo");//NEED TEST
+        feedServo = hardwareMap.get(CRServo.class, "feedServo");//NEED TEST
         guideMotor = hardwareMap.get(DcMotor.class, "guideMotor");//NEED ADD
         accelMotor = hardwareMap.get(DcMotor.class, "accelMotor");//NEED ADD
 
@@ -46,9 +50,7 @@ public void runOpMode() {
         rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         double x;
         double y;
-        boolean trigger;
-        boolean armGuide;
-        boolean armAccel;
+        double z;
         
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -57,15 +59,15 @@ public void runOpMode() {
 
         while (opModeIsActive()) {
 
-        if (gamaepad1.dpad_up == false && dpad_upPrevious == true){armGuide = !armGuide;}
-        dpad_upPrevious = gamaepad1.dpad_up;
-        if (gamaepad1.dpad_down == false && dpad_downPrevious == true){armAccel = !armAccel;}
-        dpad_downPrevious = gamaepad1.dpad_down;
+        if (gamepad1.dpad_up == false && dpad_upPrevious == true){armGuide = !armGuide;}
+        dpad_upPrevious = gamepad1.dpad_up;
+        if (gamepad1.dpad_down == false && dpad_downPrevious == true){armAccel = !armAccel;}
+        dpad_downPrevious = gamepad1.dpad_down;
         
         trigger = gamepad1.a;
         x = gamepad1.left_stick_x;
         y = -gamepad1.left_stick_y - gamepad1.right_stick_y;
-        z = gamaepad1.right_stick_x;
+        z = gamepad1.right_stick_x;
 
         if (armGuide){guideMotor.setPower(guideSpeed);}
         else{guideMotor.setPower(0);}
@@ -82,6 +84,9 @@ public void runOpMode() {
         leftBackMotor.setPower(y+x+z);
         
         telemetry.addData("Status", "Running");
+        telemetry.addData("GuideMotor", armGuide);
+        telemetry.addData("Accelorator", armAccel);
+        telemetry.addData("Trigger", trigger);
         telemetry.update();
         }
     }
